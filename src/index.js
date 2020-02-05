388e0e574783b2bf3f457da3dedb60dc873709ed
JimvandeVen <Jim.van.de.Ven@hva.nl>
2020-02-05 09:46:43 +0100

    almost finnished

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import LabelList from "./components/LabelList";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Container = () => {
+  const items = ["Foo", "Bar", "Baz", "Qux"];
+  return <LabelList items={items} />;
+};
+
+ReactDOM.render(<Container />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
