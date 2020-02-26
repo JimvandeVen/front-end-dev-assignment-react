2d198f066bb485cb33600036ae0a05217bb8c575
JimvandeVen <Jim.van.de.Ven@hva.nl>
2020-02-26 11:21:16 +0100

    LabelList is now a function component

--- src/components/LabelList.jsx
+++ src/components/LabelList.jsx
@@ -3,37 +3,9 @@ import PropTypes from "prop-types";
 import "../App.css";
 import LabelForm from "./LabelForm.jsx";
 
-// const LabelList = labels => {
-//   return <ul>{console.log(labels)}</ul>;
-// };
-
-class LabelList extends React.Component {
-  /*
-  handleEditForm = (label, index) => {
-    console.log("label =", label);
-    
-    this.setState({
-      ...this.state,
-      showEdit: false,
-      showAddNew: true
-    });
-    const newLabels = R.update(index, label, this.props.labels);
-    this.props.onChangeLabels(newLabels);
-  };
-  */
-
-  /*handleAddForm(label) {
-    this.setState({
-      ...this.state,
-      showEdit: false,
-      showAddNew: true
-    });
-    const newLabels = R.append(label, this.props.labels);
-    this.props.onChangeLabels(newLabels);
-  }*/
-
-  render() {
-    const labels = this.props.labels.map((label, index) => (
+const LabelList = props => (
+  <ul className="list-group">
+    {props.labels.map((label, index) => (
       <li
         className="list-group-item d-flex justify-content-between align-items-center"
         key={index}
@@ -45,22 +17,22 @@ class LabelList extends React.Component {
           {label.name}
         </span>
 
-        {index !== this.props.showEdit && (
+        {index !== props.showEdit && (
           <button
             className="btn btn-sm btn-link editButton"
-            onClick={() => this.props.onRequestEditLabel(index)}
+            onClick={() => props.onRequestEditLabel(index)}
           >
             Edit
           </button>
         )}
 
-        {index === this.props.showEdit && (
+        {index === props.showEdit && (
           <div>
             <button
               type="button"
               className="close"
               aria-label="Close"
-              onClick={this.props.onCloseLabelForm}
+              onClick={props.onCloseLabelForm}
             >
               <span aria-hidden="true">&times;</span>
             </button>
@@ -68,45 +40,39 @@ class LabelList extends React.Component {
             <LabelForm
               index={index}
               label={label}
-              onSubmit={label => this.props.onEditLabel(label, index)}
+              onSubmit={label => props.onEditLabel(label, index)}
             />
           </div>
         )}
       </li>
-    ));
-
-    return (
-      <ul className="list-group">
-        {labels}
-        {this.props.showAddForm ? (
-          <li className="list-group-item d-flex justify-content-between align-items-center">
-            <div>
-              <button
-                type="button"
-                className="close"
-                aria-label="Close"
-                onClick={this.props.onCloseLabelForm}
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
-              <LabelForm onSubmit={this.props.onAddLabel} />
-            </div>
-          </li>
-        ) : (
-          <li className="list-group-item d-flex justify-content-between align-items-center">
-            <button
-              type="button"
-              className="btn btn-link pl-0"
-              onClick={this.props.onRequestAddLabel}
-            >
-              + add new
-            </button>
-          </li>
-        )}
-      </ul>
-    );
-  }
-}
+    ))}
+    {props.showAddForm ? (
+      <li className="list-group-item d-flex justify-content-between align-items-center">
+        <div>
+          <button
+            type="button"
+            className="close"
+            aria-label="Close"
+            onClick={props.onCloseLabelForm}
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+          <LabelForm onSubmit={props.onAddLabel} />
+        </div>
+      </li>
+    ) : (
+      <li className="list-group-item d-flex justify-content-between align-items-center">
+        <button
+          type="button"
+          className="btn btn-link pl-0"
+          onClick={props.onRequestAddLabel}
+        >
+          + add new
+        </button>
+      </li>
+    )}
+  </ul>
+);
 
 LabelList.propTypes = {
   labels: PropTypes.arrayOf(PropTypes.object),
